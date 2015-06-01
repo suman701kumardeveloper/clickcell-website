d239a03acbd3cfa8886d32b39a5e8e7209803e0c
Harold Robson <harold.robson@fourth.com>
2015-06-01 17:40:45 +0100

    add models

---
+++ clickcell/Controllers/ImagesController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace clickcell.Controllers
+{
+    public class ImagesController : ApiController
+    {
+        //Use PUT when you can update a resource completely through a specific resource. For instance, 
+        //if you know that an article resides at http://example.org/article/1234, you can PUT a new resource representation of this article directly through a PUT on this URL.
+        //If you do not know the actual resource location, for instance, when you add a new article, but do not have any idea where to store it,
+        //you can POST it to an URL, and let the server decide the actual URL.
+        //- See more at: http://restcookbook.com/HTTP%20Methods/put-vs-post/#sthash.kkHaAmrh.dpuf
+
+        // GET api/images
+        //if you do a get request to root + api/images it will hit this method and return wahtever the method returns if you say you want xml it will return xml if you say json you get json
+        // www.clickcell.co.uk/api/images
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/images/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/images
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/images/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/images/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ clickcell/Models/ImageModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace clickcell.Models
+{
+    public class ImageContext : DbContext
+    {
+        public DbSet<Image> Images { get; set; }
+    }
+
+    public class Image
+    {
+        public int ID { get; set; }
+        public int ViewCount { get; set; }
+        public int ExpandedViewCount { get; set; }
+        public int HideCount { get; set; }
+        public string Title { get; set; }
+        public string URI { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public List<int> CategoryIDs { get; set; }
+    }
+    public class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
